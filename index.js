bca4de3fa7d02b10f5564406098eac7c7769c475
divyajeetsingh <divyajeetsingh33@gmail.com>
2019-08-04 13:53:10 +0530

    add socket backend  code

--- index.js
+++ index.js
@@ -1,12 +1,64 @@
-//Express  js with  hello world  print 
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+io.origins("*:*")
+// io.origins('http://localhost:8100/*')
 
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', function( req,  res){
-
-res.send('Hello world')
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+    
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+    
+
+    io.on('connection', function(socket){
+        socket.on('chat message', function(msg){
+           io.emit('chat message', msg);
+        socket.broadcast.emit('hi');
+         });
+         socket.on('disconnect', function(){
+           console.log('user disconnected');
+         });
+        });
+        
+
+app.get('/', function(req, res){
+ res.sendFile(__dirname + '/index.html');
+  // return res.send({message:'true'})
+});
+
+
+http.listen(3000, function(){
+ console.log('listening on *:3000');
+});
+
+
+// let app = require('express')();
+// let http = require('http').Server(app);
+// let io = require('socket.io')(http);
+ 
+// io.on('connection', (socket) => {
+  
+//   socket.on('disconnect', function(){
+//     io.emit('users-changed', {user: socket.nickname, event: 'left'});   
+//   });
+ 
+//   socket.on('set-nickname', (nickname) => {
+//     socket.nickname = nickname;
+//     io.emit('users-changed', {user: nickname, event: 'joined'});    
+//   });
+  
+//   socket.on('add-message', (message) => {
+//       console.log(message , "")
+//     io.emit('message', {text: message.text, from: socket.nickname, created: new Date()});    
+//   });
+// });
+ 
+// var port = process.env.PORT || 3000;
+ 
+// http.listen(port, function(){
+//    console.log('listening in http://localhost:' + port);
+// });
